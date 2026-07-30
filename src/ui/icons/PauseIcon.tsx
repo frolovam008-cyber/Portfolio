89d78a35ac96dab78f336cde9b602153234899bd
Marina Frolova <vmv1984@yandex.ru>
2026-07-30 23:46:44 +0300

    deployment

--- src/ui/icons/PauseIcon.tsx
+++
@@ -1,6 +0,0 @@
-import { createIcon } from "../createIcon";
-
-export const PauseIcon = createIcon({
-  viewBox: "0 0 24 24",
-  content: <path d="M6 3H8V21H6V3ZM16 3H18V21H16V3Z" fill="currentColor" />,
-});
